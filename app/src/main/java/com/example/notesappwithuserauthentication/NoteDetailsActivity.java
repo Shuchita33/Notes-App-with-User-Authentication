f52f8dd7e6a60d43872f7d1a3cbf69f3c6f69a05
Shuchita_33 <shuchi2133@gmail.com>
2023-05-11 13:17:20 +0530

    functionality of add note page

--- app/src/main/java/com/example/notesappwithuserauthentication/NoteDetailsActivity.java
+++ app/src/main/java/com/example/notesappwithuserauthentication/NoteDetailsActivity.java
@@ -3,12 +3,27 @@ package com.example.notesappwithuserauthentication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class NoteDetailsActivity extends AppCompatActivity {
+    EditText titleEditText,contentEditText;
+    ImageButton saveNoteBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_details);
+        titleEditText=findViewById(R.id.notes_title_text);
+        saveNoteBtn=findViewById(R.id.save_note_btn);
+        saveNoteBtn.setOnClickListener((v)->saveNote());
     }
+    void saveNote(){
+        String noteTitle=titleEditText.getText().toString();
+        String noteContent=contentEditText.getText().toString();
+        if(noteTitle.isEmpty()){
+            titleEditText.setError("Title of the note is required ");
+        }
+    }
+
 }
