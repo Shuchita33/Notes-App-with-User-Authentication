d29b6a30800c9c489200a65fd7a24714f264169f
Yash Agrahari <yashagrahari456@gmail.com>
2023-05-18 01:39:45 +0530

    Working Function of Logout and Delete Notes

--- app/src/main/java/com/example/notesappwithuserauthentication/NoteDetailsActivity.java
+++ app/src/main/java/com/example/notesappwithuserauthentication/NoteDetailsActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.widget.TextViewCompat;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -15,88 +16,106 @@ import com.google.firebase.Timestamp;
 import com.google.firebase.firestore.DocumentReference;
 
 public class NoteDetailsActivity extends AppCompatActivity {
+
     EditText titleEditText,contentEditText;
     ImageButton saveNoteBtn;
     TextView pageTitleTextView;
     String title,content,docId;
-    boolean isEditMode=false;
+    boolean isEditMode = false;
+    TextView deleteNoteTextViewBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_details);
 
-        titleEditText= (EditText) findViewById(R.id.notes_title_text);
-        saveNoteBtn=findViewById(R.id.save_note_btn);
-        pageTitleTextView=findViewById(R.id.page_title);
+        titleEditText = findViewById(R.id.notes_title_text);
+        contentEditText = findViewById(R.id.notes_content_text);
+        saveNoteBtn = findViewById(R.id.save_note_btn);
+        pageTitleTextView = findViewById(R.id.page_title);
+        deleteNoteTextViewBtn  = findViewById(R.id.delete_note_text_view_btn);
 
         //receive data
-        title=getIntent().getStringExtra("title");
-        content=getIntent().getStringExtra("content");
-        docId=getIntent().getStringExtra("docId");
+        title = getIntent().getStringExtra("title");
+        content= getIntent().getStringExtra("content");
+        docId = getIntent().getStringExtra("docId");
 
-        if(docId!=null && !docId.isEmpty())
-        {
-            isEditMode=true;
+        if(docId!=null && !docId.isEmpty()){
+            isEditMode = true;
         }
 
         titleEditText.setText(title);
         contentEditText.setText(content);
-
-        if(isEditMode)
-        {
-            pageTitleTextView.setText("Edit your notes");
+        if(isEditMode){
+            pageTitleTextView.setText("Edit your note");
+            deleteNoteTextViewBtn.setVisibility(View.VISIBLE);
         }
 
-        saveNoteBtn.setOnClickListener((v)->saveNote());
+        saveNoteBtn.setOnClickListener( (v)-> saveNote());
+
+        deleteNoteTextViewBtn.setOnClickListener((v)-> deleteNoteFromFirebase() );
 
     }
 
-
     void saveNote(){
-
-        String noteTitle=titleEditText.getText().toString();
-        String noteContent=contentEditText.getText().toString();
-        if(noteTitle.isEmpty()){
-            titleEditText.setError("Title of the note is required ");
+        String noteTitle = titleEditText.getText().toString();
+        String noteContent = contentEditText.getText().toString();
+        if(noteTitle==null || noteTitle.isEmpty() ){
+            titleEditText.setError("Title is required");
             return;
         }
-
-
         Note note = new Note();
         note.setTitle(noteTitle);
         note.setContent(noteContent);
         note.setTimestamp(Timestamp.now());
 
         saveNoteToFirebase(note);
+
     }
 
     void saveNoteToFirebase(Note note){
         DocumentReference documentReference;
-
-        if(isEditMode)
-        {
+        if(isEditMode){
             //update the note
             documentReference = Utility.getCollectionReferenceForNotes().document(docId);
-        }
-        else
-        {
+        }else{
             //create new note
             documentReference = Utility.getCollectionReferenceForNotes().document();
         }
+
+
 
         documentReference.set(note).addOnCompleteListener(new OnCompleteListener<Void>() {
             @Override
             public void onComplete(@NonNull Task<Void> task) {
                 if(task.isSuccessful()){
-                    Utility.showToast(NoteDetailsActivity.this,"Note Added Successfully");
+                    //note is added
+                    Utility.showToast(NoteDetailsActivity.this,"Note added successfully");
                     finish();
-                }
-                else{
+                }else{
                     Utility.showToast(NoteDetailsActivity.this,"Failed while adding note");
                 }
             }
         });
+
     }
+
+    void deleteNoteFromFirebase(){
+        DocumentReference documentReference;
+        documentReference = Utility.getCollectionReferenceForNotes().document(docId);
+        documentReference.delete().addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful()){
+                    //note is deleted
+                    Utility.showToast(NoteDetailsActivity.this,"Note deleted successfully");
+                    finish();
+                }else{
+                    Utility.showToast(NoteDetailsActivity.this,"Failed while deleting note");
+                }
+            }
+        });
+    }
+
 
 }
