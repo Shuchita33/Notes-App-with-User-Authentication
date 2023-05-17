9626ee7b9021dae1b8e0077630321da5ddfd51e5
Shuchita_33 <shuchi2133@gmail.com>
2023-05-17 20:34:48 +0530

    function in MainActivity to set recycler view

--- app/src/main/java/com/example/notesappwithuserauthentication/MainActivity.java
+++ app/src/main/java/com/example/notesappwithuserauthentication/MainActivity.java
@@ -1,17 +1,27 @@
 package com.example.notesappwithuserauthentication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.ImageButton;
 import android.widget.PopupMenu;
 
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.Query;
+
 
 public class MainActivity extends AppCompatActivity {
     FloatingActionButton addNoteBtn;
+    RecyclerView recyclerView;
+    ImageButton menuBtn;
+    NoteAdapter noteAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,7 +31,32 @@ public class MainActivity extends AppCompatActivity {
         addNoteBtn.setOnClickListener((v)->startActivity(new Intent(MainActivity.this,NoteDetailsActivity.class)));
 
     }
+    void setupRecyclerView(){
+        Query query  = Utility.getCollectionReferenceForNotes().orderBy("timestamp",Query.Direction.DESCENDING);
+        FirestoreRecyclerOptions<Note> options = new FirestoreRecyclerOptions.Builder<Note>()
+                .setQuery(query,Note.class).build();
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        noteAdapter = new NoteAdapter(options,this);
+        recyclerView.setAdapter(noteAdapter);
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        noteAdapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        noteAdapter.stopListening();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        noteAdapter.notifyDataSetChanged();
+    }
 
 
 
