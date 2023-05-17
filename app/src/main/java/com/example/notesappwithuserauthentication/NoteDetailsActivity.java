101d570a40d984bdbbdedf6e6ec5b4a8afbf19c5
Shuchita_33 <shuchi2133@gmail.com>
2023-05-17 20:22:35 +0530

    recycler view functionality

--- app/src/main/java/com/example/notesappwithuserauthentication/NoteDetailsActivity.java
+++ app/src/main/java/com/example/notesappwithuserauthentication/NoteDetailsActivity.java
@@ -2,10 +2,12 @@ package com.example.notesappwithuserauthentication;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.widget.TextViewCompat;
 
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -20,11 +22,17 @@ public class NoteDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_details);
-        titleEditText=findViewById(R.id.notes_title_text);
+
+        titleEditText= (EditText) findViewById(R.id.notes_title_text);
         saveNoteBtn=findViewById(R.id.save_note_btn);
+
         saveNoteBtn.setOnClickListener((v)->saveNote());
+
     }
+
+
     void saveNote(){
+
         String noteTitle=titleEditText.getText().toString();
         String noteContent=contentEditText.getText().toString();
         if(noteTitle.isEmpty()){
